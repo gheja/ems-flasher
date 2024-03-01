gheja/ems-flasher

commit 8a1141b7e07804e2b6d9ae77962b02f3bf222215

Correct header checksum, add debug when checksum OK

main.c
@@ -8,7 +8,7 @@
 #include "ems.h"
 
 // don't forget to bump this :P
-#define VERSION "0.02"
+#define VERSION "0.04"
 
 // one bank is 32 megabits
 #define BANK_SIZE 0x400000
@@ -352,12 +352,15 @@ int main(int argc, char **argv) {
         //Verify cartridge header checksum while we're at it
         uint8_t calculated_chk = 0;
         for (i = HEADER_TITLE; i < HEADER_CHKSUM; i++) {
-            calculated_chk -= buf[i] - 1;
+            calculated_chk -= buf[i] + 1;
         }
 
         if (calculated_chk != buf[HEADER_CHKSUM]) {
             printf("Cartridge header checksum invalid. This game will NOT boot on real hardware.\n");
-        }
+        } else {
+	    if (opts.verbose) 
+	        printf("Cartridge header checksum OK.\n");
+	}
 
         if (buf[HEADER_SGBFLAG] == 0x03 && buf[HEADER_OLDLICENSEE] != 0x33) {
             printf("SGB functions were enabled, but Old Licensee field is not set to 33h. This game will not be able to use SGB functions on real hardware.\n");
@@ -417,12 +420,15 @@ int main(int argc, char **argv) {
         //Verify cartridge header checksum while we're at it
         calculated_chk = 0;
         for (i = HEADER_TITLE; i < HEADER_CHKSUM; i++) {
-            calculated_chk -= buf[i] - 1;
+            calculated_chk -= buf[i] + 1;
         }
 
         if (calculated_chk != buf[HEADER_CHKSUM]) {
             printf("Cartridge header checksum invalid. This game will NOT boot on real hardware.\n");
-        }
+        } else {
+	    if (opts.verbose) 
+	        printf("Cartridge header checksum OK.\n");
+	}
 
         if (buf[HEADER_SGBFLAG] == 0x03 && buf[HEADER_OLDLICENSEE] != 0x33) {
             printf("SGB functions were enabled, but Old Licensee field is not set to 33h. This game will not be able to use SGB functions on real hardware.\n");

commit 2ff0b1e50e569854285dbd8489517b7921920978

Calculate header checksum correctly
The old header checksum code never worked. First it calculated checksum
incorrectly due to wrong algorithm, then we applied an incorrect fix in
2014 based on an incorrect GitHub comment.

Calculate the header checksum correctly as per community documentation:

* https://gbdev.io/pandocs/The_Cartridge_Header.html
* https://gbdev.gg8.se/wiki/articles/The_Cartridge_Header

The code is used multiple times so move it to a separate function to
remove duplication of code.

Tested by flashing good Redump ROMs to the cart and also verifying the
ROMs are correct with RGBDS's rgbfix.

Fixes: 8a1141b7e078 ("Correct header checksum, add debug when checksum OK")
Signed-off-by: Jamie Bainbridge <jamie.bainbridge@gmail.com>

main.c
@@ -8,7 +8,7 @@
 #include "ems.h"
 
 // don't forget to bump this :P
-#define VERSION "0.04"
+#define VERSION "0.05"
 
 // one bank is 32 megabits
 #define BANK_SIZE 0x400000
@@ -212,6 +212,15 @@ mode_error2:
     usage(argv[0]);
 }
 
+uint8_t rom_checksum_header(unsigned char *rom)
+{
+        uint8_t checksum = 0;
+        for (uint16_t address = HEADER_TITLE; address < HEADER_CHKSUM; address++) {
+            checksum = checksum - rom[address] - 1;
+        }
+	return checksum;
+}
+
 /**
  * Main
  */
@@ -350,10 +359,7 @@ int main(int argc, char **argv) {
         }
 
         //Verify cartridge header checksum while we're at it
-        uint8_t calculated_chk = 0;
-        for (i = HEADER_TITLE; i < HEADER_CHKSUM; i++) {
-            calculated_chk -= buf[i] + 1;
-        }
+        uint8_t calculated_chk = rom_checksum_header(buf);
 
         if (calculated_chk != buf[HEADER_CHKSUM]) {
             printf("Cartridge header checksum invalid. This game will NOT boot on real hardware.\n");
@@ -418,10 +424,7 @@ int main(int argc, char **argv) {
         }
 
         //Verify cartridge header checksum while we're at it
-        calculated_chk = 0;
-        for (i = HEADER_TITLE; i < HEADER_CHKSUM; i++) {
-            calculated_chk -= buf[i] + 1;
-        }
+        calculated_chk = rom_checksum_header(buf);
 
         if (calculated_chk != buf[HEADER_CHKSUM]) {
             printf("Cartridge header checksum invalid. This game will NOT boot on real hardware.\n");